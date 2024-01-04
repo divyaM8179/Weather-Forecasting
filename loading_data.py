0afa99723c2855f19af86869058df7f42029f5dd
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2024-01-04 18:35:28 +0530

    Update loading_data.py

--- loading_data.py
+++ loading_data.py
@@ -1,7 +1,7 @@
 import  pandas as pd
 
 def loading_data():
-    data = pd.read_csv('./testset.csv', index_col='datetime_utc')
+    data = pd.read_csv('testset.csv', index_col='datetime_utc')
     data.index = pd.to_datetime(data.index)
     return data
 
